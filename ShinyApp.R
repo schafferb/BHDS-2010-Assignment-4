3a55cfa9d440a9955f9b24684fb28b1d6b379c8c
Bryanna <schafferbryanna22@gmail.com>
2025-11-21 11:17:42 -0800

    added table for proportions/summary statistics

--- ShinyApp.R
+++ ShinyApp.R
@@ -56,7 +56,38 @@ ui <- fluidPage(
                        )
              ),
     tabPanel(strong("Data Exploration"),
-             ),
+             br(),
+             mainPanel(width = 12,
+                       h3(strong("Coronary Heart Disease Data Exploration"), align = "center"),
+                       textOutput("howtodata"),
+                       br(),
+                       
+                       # dropdown
+                       selectInput("independent", strong("Select a Variable of Interest"),
+                                   choices = c("Systolic Blood Pressure" = "sbp",
+                                               "Tobacco" = "tobacco",
+                                               "Low Density Lipoprotein Cholesterol" = "ldl",
+                                               "Measure of Body Fat" = "adiposity",
+                                               "Family History of Heart Disease" = "famhist",
+                                               "Obesity Index" = "obesity",
+                                               "Alcohol Consumption" = "alcohol",
+                                               "Age" = "age"),
+                                   selected = "sbp"),
+                       
+                       br(),
+                       
+                       # table output
+                       strong(tableOutput("summaryTable"), align = "center")
+             ), tags$head(
+               tags$style(HTML("
+    #summaryTable table {
+      font-size: 22px;
+    }
+    #summaryTable th, #summaryTable td {
+      padding: 12px;
+    }
+  "))
+             )),
     tabPanel(strong("Data Analysis"))
   )
 )
@@ -92,6 +123,38 @@ server <- function(input, output, session){
       )
     )
   })
+  output$summaryTable <- renderTable({
+
+  var <- input$independent
+  df <- CHD
+
+  # If variable is categorical → show proportions of CHD
+  if (is.factor(df[[var]])) {
+
+    table_df <- df %>%
+      group_by(.data[[var]], chd) %>%
+      summarise(n = n(), .groups = "drop_last") %>%
+      mutate(proportion = round(n / sum(n), 3)) %>%
+      rename(Level = .data[[var]])
+
+    return(table_df)
+
+  } else {
+
+    # If numeric → show summary statistics by CHD group
+    table_df <- df %>%
+      group_by(chd) %>%
+      summarise(
+        Mean = round(mean(.data[[var]]), 2),
+        Median = round(median(.data[[var]]), 2),
+        SD = round(sd(.data[[var]]), 2),
+        Min = round(min(.data[[var]]), 2),
+        Max = round(max(.data[[var]]), 2)
+      )
+
+    return(table_df)
+  }
+})
 }
 
 
