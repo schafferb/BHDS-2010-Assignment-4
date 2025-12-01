12ade961ebc3946fdd5aeb05faf2db5c6afa5a11
IbrahimElbasheer <Ibrahim_Elbasheer@brown.edu>
2025-12-01 04:54:23 -0500

    Create the drop list and the slider bar

--- app.R
+++ app.R
@@ -1,34 +1,52 @@
 
 library(shiny)
 library(ggplot2)
+library(dplyr)
+library(openxlsx)
 
 setwd("D:/Colleges/Brown University/BHDS 2010/Assignment4/BHDS-2010-Assignment-4")
 
+data <- read.xlsx("SAheart.xlsx")
+
 ui <-fluidPage( 
-  # inputs 
-  ## text input <inputId1> 
-  textInput(...), 
-  ## select input <inputId2> 
-  selectInput(), 
-  ## text input <inputId3> 
-  # … 
-  #outputs 
-  ## data table 
-  DT::dataTableOutput("dataTable"), 
-  ## figure 
-  plotOutput("scatterPlot"), 
-) 
+  titlePanel("SA Heart Disease Data Explorer"),
+  sidebarLayout(
+    sidebarPanel( width = 4,
+      selectInput("variable", 
+                  "Select a variable:",
+               choices = c("Type A Behavior" = "typea", 
+                           "Obesity" = "obesity", 
+                           "Alcohol" = "alcohol",
+                           "Age" = "age"),
+               selected =  "age",
+      ),
+      uiOutput("slider")
+),
+mainPanel(
+  paste("About This App:"),
+    p("This shiny app uses a dataset collected from South Africa. It is the 
+      heart disease dataset, and it is collected to explore risk factors for 
+      coronary heart disease. Select the variable from the drop list and adjust 
+      the sliders to see how different risk factors affect CHD prevalence."),
+  plotOutput("chd_histogram"),
+  verbatimTextOutput("summary_stats")
+)
+))
+
 # Define server logic required to draw a histogram
+
 server <- server <-function(input, output) { 
-  ## data management 
-  Data <-reactive({... 
-  }) 
-  ## output 1. Generate data table based on filtered data 
-  output$dataTable <-DT::renderDataTable({... 
-  }) 
-  ## output 2. Generate a plot 
-  output$scatterPlot <-renderPlot({... 
+  output$slider <- renderUI({
+    var_data <- data[[input$variable]]
+    sliderInput("range", 
+                paste("Select", input$variable, "range:"),
+                min = floor(min(var_data, na.rm = TRUE)),
+                max = ceiling(max(var_data, na.rm = TRUE)),
+                value = c(floor(min(var_data, na.rm = TRUE)), 
+                          ceiling(max(var_data, na.rm = TRUE))),
+                step = 1)
   })
+ 
 }
 
 # Run the application 
